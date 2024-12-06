18f966ef954926a8cb24b97f2b8b27289dc690df
Abay <a.kosaev@inbox.ru>
2024-12-06 13:18:24 +0500

    feat: home route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import React, {Suspense} from "react";
-import {Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import DefaultLayout from "@/app/layout/default.tsx";
 import Loader from "@/shared/ui/Loader/Loader.tsx";
+import Home from "@/pages/home";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <Suspense fallback={<Loader/>}>
             <DefaultLayout>
                 <Routes>
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </DefaultLayout>
         </Suspense>
